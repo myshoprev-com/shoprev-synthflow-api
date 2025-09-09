46d0484fbe0bf2e48c0fa6ac817bfae0a29cb194
Jesstin Swadley <contact@jesstin.com>
2025-09-09 16:15:15 -0700

    make basic express route endpoints

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,24 @@
-import express, { type Express } from "express";
+import express, { type Express, type Request, type Response} from "express";
 
 const PORT = process.env.PORT || 3000;
 const app: Express = express();
+
+// Routes
+app.post("/", (req: Request, res: Response) => {
+	res.status(200).send("POST request endpoint");
+});
+
+app.get("/", (req: Request, res: Response) => {
+	res.status(200).send("GET request endpoint");
+});
+
+app.patch("/", (req: Request, res: Response) => {
+	res.status(200).send("PATCH request endpoint");
+});
+
+app.delete("/", (req: Request, res: Response) => {
+	res.status(200).send("DELETE request endpoint");
+});
 
 app.listen(PORT, () => {
 	console.log(`Server is running on Port: ${PORT}`)
